65c0244f716c020d132a1d5520a198db03842c84
Eli Itah <eli.itah@tempus.com>
2025-01-03 10:55:23 -0600

    Exercises 4 with primitives

--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -11,7 +11,10 @@ mod tests {
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
         // let second = ???;
-
+        // this is an equally valid way, but we're trying to learn about
+        // indexing into slices
+        // let (_, second, _) = numbers;
+        let second = numbers.1;
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
 }
